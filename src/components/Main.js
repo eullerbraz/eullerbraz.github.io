e9ba4ce60a4e7a30d6745411fa2e31b28d5954d7
Euller Braz <euller.braz99@gmail.com>
2021-12-27 15:01:54 -0300

    Adicionado componentes ao app

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AboutSection from './AboutSection';
+import HeroSection from './HeroSection';
+import TechSection from './TechSection';
+
+const Main = () => {
+  return (
+    <main>
+      <HeroSection />
+      <AboutSection />
+      <TechSection />
+    </main>
+  );
+}
+
+export default Main;
